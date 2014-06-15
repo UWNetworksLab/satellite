963a45ef5aa0f41101d848587a7b6b24cb2874f6
Will Scott <willscott@gmail.com>
2014-06-15 14:14:26 -0700

    Add filter to provide basic stats & filter zmap output to working hosts

---
+++ filter.js
@@ -0,0 +1,56 @@
+var fs = require('fs'),
+    readline = require('readline'),
+    stream = require('stream'),
+    dns = require('native-dns-packet');
+    
+
+var hosts = 0, recursive = 0, answer = 0, valid = 0;
+
+function printStats() {
+  console.log("l=" + hosts + ", r=" + recursive + ", a=" + answer +
+      ", v=" + valid);
+}
+
+var input = fs.createReadStream(process.argv[2]);
+var output = fs.createWriteStream(process.argv[3]);
+
+var rl = readline.createInterface({
+    input: input,
+    output: output,
+    terminal: false
+});
+
+rl.on('line', function(line) {
+  var info = line.split(','),
+      record;
+  if (info.length != 3) {
+    return;
+  }
+  hosts += 1;
+  record = dns.parse(new Buffer(info[2], 'hex'));
+  
+  if (record.header.qr == 0 || record.header.ra == 0 ) {
+    return;
+  }
+
+  recursive += 1;
+
+  output.write(info[0] + '\n');
+
+  if (record.answer.length == 0) {
+    return;
+  }
+  answer += 1;
+
+  if (record.answer[0].address == '128.208.3.200') {
+    valid += 1;
+  }
+});
+
+rl.on('close', function() {
+  printStats();
+  output.end();
+  output.on('finish', function() {
+    process.exit(0);
+  });
+});
