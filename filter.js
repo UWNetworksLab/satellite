d3229ebe94395d85c1eea16231326e6c101771aa
Will Scott <willscott@gmail.com>
2014-06-16 17:34:22 -0700

    try piping for perf

--- filter.js
+++ filter.js
@@ -4,6 +4,28 @@ var fs = require('fs'),
     dns = require('native-dns-packet');
     
 
+/** Line chunker from http://strongloop.com/strongblog/practical-examples-of-the-new-node-js-streams-api/ */
+var liner = new stream.Transform( { objectMode: true } )
+
+liner._transform = function (chunk, encoding, done) {
+     var data = chunk.toString()
+     if (this._lastLineData) data = this._lastLineData + data
+
+     var lines = data.split('\n')
+     this._lastLineData = lines.splice(lines.length-1,1)[0]
+
+     lines.forEach(this.push.bind(this))
+     done()
+}
+
+liner._flush = function (done) {
+     if (this._lastLineData) this.push(this._lastLineData)
+     this._lastLineData = null
+     done()
+}
+/** end line chunker */
+
+
 var hosts = 0, recursive = 0, answer = 0, valid = 0;
 
 function printStats() {
@@ -14,34 +36,33 @@ function printStats() {
 var input = fs.createReadStream(process.argv[2]);
 var output = fs.createWriteStream(process.argv[3]);
 
-var rl = readline.createInterface({
-    input: input,
-    output: output,
-    terminal: false
-});
-
-rl.on('line', function(line) {
+var watcher = new stream.Transform( { objectMode: true } );
+watcher._transform = function (line, encoding, done) {
   var info = line.split(','),
       record;
   if (info.length != 3) {
+    done();
     return;
   }
   hosts += 1;
   try {
     record = dns.parse(new Buffer(info[2], 'hex'));
   } catch(e) {
+    done();
     return;
   }
   
   if (record.header.qr == 0 || record.header.ra == 0 ) {
+    done();
     return;
   }
 
   recursive += 1;
 
-  output.write(info[0] + '\n');
+  this.push(info[0] + '\n');
 
   if (record.answer.length == 0) {
+    done();
     return;
   }
   answer += 1;
@@ -49,12 +70,13 @@ rl.on('line', function(line) {
   if (record.answer[0].address == '128.208.3.200') {
     valid += 1;
   }
-});
 
-rl.on('close', function() {
+  done();
+};
+
+input.pipe(liner).pipe(watcher).pipe(output);
+
+output.on('finish', function() {
   printStats();
-  output.end();
-  output.on('finish', function() {
-    process.exit(0);
-  }); 
-});
+  process.exit(0);
+}); 
