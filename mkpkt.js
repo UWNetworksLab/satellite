ff2af82d55ac33419b612b8f10eaf7689464cb4e
Will Scott <willscott@gmail.com>
2014-07-07 12:30:47 -0700

    allow mkpkt to be required

--- mkpkt.js
+++ mkpkt.js
@@ -1,16 +1,24 @@
 var dns = require('native-dns-packet');
 var fs = require('fs');
 
-var type = 1;
+var mkPkt = function(domain, file) {
+  var type = 1;
 
-var packet = new dns();
-packet.header.id = Math.round(Math.random() * 0xffff);
-packet.question.push({
-  name: process.argv[3],
-  type: type,
-  class: 1
-});
-var buffer = new Buffer(200);
-var len = dns.write(buffer, packet);
-var prefix = buffer.slice(0, len);
-fs.writeFile(process.argv[2], prefix);
+  var packet = new dns();
+  packet.header.id = Math.round(Math.random() * 0xffff);
+  packet.question.push({
+    name: domain,
+    type: type,
+    class: 1
+  });
+  var buffer = new Buffer(200);
+  var len = dns.write(buffer, packet);
+  var prefix = buffer.slice(0, len);
+  fs.writeFile(file, prefix);
+}
+
+if (!module.parent) {
+  mkPkt(process.argv[3], process.argv[2]);
+} else {
+  exports.make = mkPkt;
+}
