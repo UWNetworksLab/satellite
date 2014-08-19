ae84ebd6e13af53e4ba2562c508e6bbd3608c8d7
Adam Lerner <eletari@gmail.com>
2014-08-19 13:30:45 -0700

    Clarify opening comment to csv -> \njson parser.

--- parseIPsFromPackets.js
+++ parseIPsFromPackets.js
@@ -1,5 +1,5 @@
 // Input: zmap output file (.csv of servers and base64 packets).
-// Output: .json of received packet info.
+// Output: \n delimited .json, one line per received DNS packet.
 var util = require('util');
 var fs = require('fs'),
     readline = require('readline'),
