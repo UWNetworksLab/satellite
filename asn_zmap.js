3daab4b4862b8db86e7f7317974a5edc716c3ecb
Will Scott <willscott@gmail.com>
2014-11-17 16:24:09 -0500

    asn_zmap does things

--- asn_zmap.js
+++ asn_zmap.js
@@ -20,3 +20,32 @@ if (!process.argv[3]) {
   process.exit(1);
 }
 
+function addLine(map, into, line) {
+  var theasn = map.lookup(parts[0].trim());
+  if (!into[theasn]) {
+    into[theasn] = 0;
+  }
+  into[theasn] += 1;
+}
+
+function dealWithFile(map, infile, outfile) {
+  var into = {};
+  return Q.Promise(function(resolve, reject) {
+    fs.createReadStream(infile)
+      .pipe(es.split())
+      .pipe(es.mapSync(addLine.bind({}, map, into)))
+      .on('end', resolve)
+      .on('error', reject);
+  }).then(function(m) {
+    fs.writeFileSync(outfile, JSON.stringify(into));
+    delete into;
+    return true;
+  });
+}
+
+asn.getMap().then(function (map) {
+  return dealWithFile(map, infile, outfile);
+}).then(function() {
+  console.log(chalk.green('Done'));
+  process.exit(0);
+});
