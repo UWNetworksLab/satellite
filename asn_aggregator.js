2b53cb49ba3adf70d292f4bc384677214a37b859
Sidney Berg <sidberg@cs.washington.edu>
2015-03-17 19:11:09 -0700

    old aggregator output same as multi-aggregator

--- asn_aggregator.js
+++ asn_aggregator.js
@@ -6,9 +6,9 @@
 
 var Q = require('q');
 var fs = require('fs');
-var es = require('event-stream')
-var chalk = require('chalk')
-var asn = require('./asn_aggregation/asn_lookup')
+var es = require('event-stream');
+var chalk = require('chalk');
+var asn = require('./asn_aggregation/asn_lookup');
 var dns = require('native-dns-packet');
 
 if (!process.argv[4]) {
@@ -18,6 +18,8 @@ if (!process.argv[4]) {
 var rundir = process.argv[2];
 var asnTable = process.argv[3];
 var outFile = process.argv[4];
+var outFD = fs.openSync(outFile, 'ax');
+
 
 function parseDomainLine(map, into, domain, line) {
   var parts = line.toString('ascii').split(',');
@@ -26,13 +28,18 @@ function parseDomainLine(map, into, domain, line) {
   }
   var theasn = map.lookup(parts[0]);
   var record;
+
+  if (theasn === 'ZZ') {
+    theasn = 'unknown';
+  }
+
   try {
     record = dns.parse(new Buffer(parts[2], 'hex'));
     if (!into[theasn]) {
       into[theasn] = {};
     }
     if (record.answer.length > 0) {
-      record.answer.forEach(function(answer) {
+      record.answer.forEach(function (answer) {
         var ip = answer.address;
         if (!into[theasn][ip]) {
           into[theasn][ip] = 1;
@@ -46,30 +53,29 @@ function parseDomainLine(map, into, domain, line) {
       }
       into[theasn].empty += 1;
     }
-  } catch(e) {
+  } catch (e) {
     into.failed += 1;
-    return;
   }
 }
 
 // Read one csv file line by line.
 function collapseSingle(map, domain, file) {
   var into = {
+    name: domain,
     failed: 0
   };
   if (fs.existsSync(rundir + '/' + file + '.asn.json')) {
     return Q(0);
   }
 
-  return Q.Promise(function(resolve, reject) {
+  return Q.Promise(function (resolve, reject) {
     fs.createReadStream(rundir + '/' + file)
       .pipe(es.split())
       .pipe(es.mapSync(parseDomainLine.bind({}, map, into, domain)))
       .on('end', resolve)
       .on('error', reject);
-  }).then(function(m) {
-    fs.writeFileSync(rundir + '/' + file + '.asn.json', JSON.stringify(into));
-    delete into;
+  }).then(function () {
+    fs.writeSync(outFD, JSON.stringify(into) + '\n');
     return true;
   });
 }
@@ -77,82 +83,41 @@ function collapseSingle(map, domain, file) {
 function collapseAll(asm) {
   var files = fs.readdirSync(rundir);
   console.log(chalk.blue("Starting Aggregation of %d domains"), files.length);
-  return Q.Promise(function(resolve, reject) {
+  return Q.Promise(function (resolve, reject) {
     var base = Q(0);
     var n = 0;
     var allFiles = [];
-    files.forEach(function(domain) {
+    files.forEach(function (domain) {
       if (domain.indexOf('.csv') < 0 || domain.indexOf('asn.json') > 0) {
         return;
       }
       allFiles.push(domain);
       var dn = domain.split('.csv')[0];
       n += 1;
-      if (n%100 === 0) {
-        base.then(function() {
+      if (n % 100 === 0) {
+        base.then(function () {
           console.log(chalk.blue("."));
         })
       }
-      if (n%1000 === 0) {
-        base.then(function(x) {
+      if (n % 1000 === 0) {
+        base.then(function (x) {
           console.log(chalk.green(x));
-        }.bind({},n))
+        }.bind({}, n))
       }
       base = base.then(collapseSingle.bind({}, asm, dn, domain));
     });
-    return base.then(function() {
+    return base.then(function () {
       console.log(chalk.green('Done.'));
       return allFiles;
     }).then(resolve, reject);
   });
 }
 
-var queue;
-function writeMap(files) {
-  console.log(chalk.blue('Writing Compiled Map.'));
-  var stream = fs.createWriteStream(outFile);
-  return Q.Promise(function(resolve, reject) {
-    stream.on('finish', resolve);
-    stream.on('error', reject);
-
-    stream.write("{\"length\":" + files.length);
-
-    queue = files;
-    aggregateMap(stream);
-  }).then(function() {
-    console.log(chalk.green('Done.'));
-    console.log(chalk.blue('Cleaning Up.'));
-    var all = fs.readdirSync(rundir);
-    all.forEach(function(file) {
-      if (file.indexOf('.asn.json') > 0) {
-        fs.unlinkSync(rundir + '/' + file);
-      }
-    });
-  });
-}
-
-function aggregateMap(stream) {
-  if (queue.length) {
-    var next = queue.pop();
-    var domain = next.split('.csv')[0];
-    stream.write(",\"" + domain + "\":");
-    if(stream.write(fs.readFileSync(rundir + '/' + next + '.asn.json'))) {
-      process.nextTick(aggregateMap.bind({}, stream));
-    } else {
-      stream.once('drain', aggregateMap.bind({}, stream));
-    }
-  } else {
-    stream.write("}");
-    stream.end();
-  }
-}
-
 asn.getMap(asnTable)
-.then(collapseAll)
-.then(writeMap)
-.then(function() {
-  console.log(chalk.green('Done'));
-  process.exit(0);
-},function(err) {
-  console.error(chalk.red(err));
-});
+  .then(collapseAll)
+  .then(function () {
+    fs.closeSync(outFD);
+    process.exit(0);
+  }, function (err) {
+    console.error(chalk.red(err));
+  });
