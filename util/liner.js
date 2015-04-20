d64820f24dc7f095659448c00d3d80b18b47f939
Will Scott <willscott@gmail.com>
2015-04-20 11:18:29 -0700

    add factory to liner

--- util/liner.js
+++ util/liner.js
@@ -3,30 +3,34 @@
 
 /** Line chunker from http://strongloop.com/strongblog/practical-examples-of-the-new-node-js-streams-api/ */
 var stream = require('stream');
-var liner = new stream.Transform({objectMode: true});
+var makeLiner = function() {
+  var liner = new stream.Transform({objectMode: true});
 
-liner._transform = function (chunk, encoding, done) {
-  var data = chunk.toString(), lines;
-  if (this._lastLineData) {
-    data = this._lastLineData + data;
-  }
+  liner._transform = function (chunk, encoding, done) {
+    var data = chunk.toString(), lines;
+    if (this._lastLineData) {
+      data = this._lastLineData + data;
+    }
   
-  lines = data.split('\n');
-  this._lastLineData = lines.splice(lines.length - 1, 1)[0];
+    lines = data.split('\n');
+    this._lastLineData = lines.splice(lines.length - 1, 1)[0];
   
-  lines.forEach(this.push.bind(this));
-  done();
-};
+    lines.forEach(this.push.bind(this));
+    done();
+  };
 
-liner._flush = function (done) {
-  if (this._lastLineData) {
-    this.push(this._lastLineData);
-  }
-  this._lastLineData = null;
-  done();
-};
+  liner._flush = function (done) {
+    if (this._lastLineData) {
+      this.push(this._lastLineData);
+    }
+    this._lastLineData = null;
+    done();
+  };
+  return liner;
+}
 /** end line chunker */
 
 module.exports = {
-  liner: liner
+  liner: makeLiner(),
+  get: makeLiner
 };
