8eac9f4a108b0423635836e9faa2243ec50efd09
Sidney Berg <sidneysea@gmail.com>
2015-04-22 13:24:12 -0700

    serialize matrix to disk

--- asn_aggregation/build_triangle.js
+++ asn_aggregation/build_triangle.js
@@ -21,7 +21,7 @@ function collapseToClassC(table) {
   return into;
 }
 
-function getIDX(triangle, domain1, domain2) {
+function getOffset(triangle, domain1, domain2) {
   var idx1 = triangle._domains[domain1],
     idx2 = triangle._domains[domain2];
 
@@ -32,18 +32,24 @@ function getIDX(triangle, domain1, domain2) {
     idx2 = tmp;
   }
 
-  return idx1 + Math.floor(0.5 * (idx2 - 1) * idx2);
+  return (idx1 + Math.floor(0.5 * (idx2 - 1) * idx2)) * 4;
 }
 
-function buildMatrix(filename) {
-  var table = collapseToClassC(JSON.parse(fs.readFileSync(filename))),
+function lookup(domain1, domain2) {
+  if (domain1 === domain2) {
+    return undefined;
+  }
+
+  return this._buffer.readFloatLE(getOffset(this, domain1, domain2));
+}
+
+function buildMatrix(domainToIP, outprefix) {
+  var table = collapseToClassC(JSON.parse(fs.readFileSync(domainToIP)));
     domains = Object.keys(table),
     triangle = {
+      lookup: lookup,
       _domains: {},
-      _array: new Float32Array(new ArrayBuffer(Math.floor(0.5 * (domains.length - 1) * domains.length) * 4)),
-      lookup: function (domain1, domain2) {
-        return domain1 === domain2 ? undefined : this._array[getIDX(this, domain1, domain2)];
-      }
+      _buffer: new Buffer(Math.floor(0.5 * (domains.length - 1) * domains.length) * 4)
     };
 
   domains.forEach(function (domain, idx) {
@@ -51,38 +57,50 @@ function buildMatrix(filename) {
   });
 
   console.log(chalk.blue("Building Correlation Matrix"));
-  bar = new ProgressBar(':bar :percent :eta', triangle._array.length);
+  bar = new ProgressBar(':bar :percent :eta', triangle._buffer.length / 4);
 
   for (var i = 0; i < domains.length; i++) {
     for (var j = i + 1; j < domains.length; j++) {
       var a = table[domains[i]],
         b = table[domains[j]],
-        aTotal = 0,
-        bTotal = 0,
+        total = 0,
         intersection = 0;
+
+      Object.keys(a).forEach(function (classC) {
+        total += a[classC];
+      });
+
+      Object.keys(b).forEach(function (classC) {
+        total += b[classC];
+      });
 
       Object.keys(a).filter(function (classC) {
         return b[classC];
       }).forEach(function (classC) {
-        intersection += Math.min(b[classC], a[classC]);
+        intersection += a[classC] + a[classC];
       });
 
-      Object.keys(a).forEach(function (classC) {
-        aTotal += a[classC];
-      });
-
-      Object.keys(b).forEach(function (classC) {
-        bTotal += b[classC];
-      });
-
-      // TODO: how to calc this
-      triangle._array[getIDX(triangle, domains[i], domains[j])] = Math.max(intersection / aTotal, intersection / bTotal);
+      triangle._buffer.writeFloatLE(intersection / total, getOffset(triangle, domains[i], domains[j]));
 
       bar.tick();
     }
   }
 
+  if (outprefix) {
+    fs.writeFileSync(outprefix + '.bin', triangle._buffer);
+    fs.writeFileSync(outprefix + '.json', JSON.stringify(triangle._domains));
+  }
+
   return triangle;
 }
 
-exports.getMatrix = buildMatrix;
+function loadMatrix(prefix) {
+  return {
+    lookup: lookup,
+    _domains: JSON.parse(fs.readFileSync(prefix + '.json')),
+    _buffer: fs.readFileSync(prefix + '.bin')
+  }
+}
+
+exports.buildMatrix = buildMatrix;
+exports.loadMatrix = loadMatrix;
