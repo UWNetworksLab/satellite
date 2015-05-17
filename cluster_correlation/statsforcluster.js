fe96fa81cfddb5a07cf93c9cfd6468aea26eb42c
Will Scott <willscott@gmail.com>
2015-05-17 10:03:04 -0400

    get IP,ASN,Country stats for a cluster of domains.

---
+++ cluster_correlation/statsforcluster.js
@@ -0,0 +1,57 @@
+// Get stats (ASNs, Countries, #IPs) for a cluster of domains.
+
+var fs = require('fs');
+var chalk = require('chalk');
+var ip2country = require('ip2country');
+
+// statsforcluster.js <domains.txt> <scores> <asntable> [threshold]
+
+var domains = fs.readFileSync(process.argv[2]).split('\n');
+var scores = JSON.parse(fs.readFileSync(process.argv[3]));
+var lookup = require('../asn_aggregation').loadASMap(process.argv[4]);
+
+var data = lookup.then(function (map) {
+  console.log(chalk.blue('Files Loaded.'));
+  var ips = {};
+  domains.forEach(function (dom) {
+    if (!dom) { return; }
+    scores[dom].forEach(function (ip) {
+      if (!ips[ip]) {
+        ips[ip] = [];
+      }
+      ips[ip].push(scores[dom][ip]);
+    });
+  });
+  console.log(chalk.blue(Object.keys(ips).length) + chalk.yellow(' IPs under consideration'));
+  var goodIPs = [];
+  var threshold = process.argv[5] || '1';
+
+  Object.keys(ips).forEach(function (ip) {
+    var thold = eval(threshold.replace('n', ips[ip].length));
+    if (ips[ip].reduce(add, 0) > thold) {
+      goodIPs.push(ip);
+    }
+  });
+  console.log(chalk.blue(goodIPs.length) + chalk.yellow(' In Cluster.'))
+
+  var ASNs = {}, countries = {};
+  goodIPs.forEach(function (ip) {
+    var asn = map.lookup(Number(ip));
+    var country = ip2country(Number(ip))
+    ASNs[asn] = true;
+    countries[country] = true;
+  });
+  ASNs = Object.keys(ASNs);
+  console.log(chalk.blue(ASNs.length) + chalk.yellow(' ASNs.'));
+
+  countries = Object.keys(countries);
+  console.log(chalk.blue(countries.length) + chalk.yellow(' Countries.'));
+
+  return {
+    ips: goodIPs,
+    asns: ASNs,
+    countries: countries
+  };
+});
+
+module.exports = data;
