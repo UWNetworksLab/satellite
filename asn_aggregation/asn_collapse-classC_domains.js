eb36833f9e85ac5d2abbf20988f2cb0e76eba82c
Will Scott <willscott@gmail.com>
2015-05-19 14:54:48 -0400

    add asn aggregation

--- asn_aggregation/asn_collapse-classC_domains.js
+++ asn_aggregation/asn_collapse-classC_domains.js
@@ -2,7 +2,7 @@
  * Collapse. From:
  * {domain -> {asn -> {ip -> #resolutions}}}
  * to:
- * {classC -> {domain -> #resolutions}}  &&  {domain -> {classC -> #resolutions}}
+ * {classC -> {domain -> #resolutions}}  &&  {domain -> {classC -> #resolutions}} && {domain -> {asn -> #resolutions}}
  */
 
 var fs = require('fs');
@@ -22,7 +22,7 @@ var outFile1 = process.argv[3];
 var outFile2 = process.argv[4];
 
 
-function doDomain(into, line) {
+function doDomain(into, asns, line) {
   var asn_ip,
     domain;
 
@@ -32,10 +32,12 @@ function doDomain(into, line) {
     return;
   }
   domain = asn_ip.name;
+  asns[domain] = {};
 
   Object.keys(asn_ip).filter(function (asn) {
     return typeof asn_ip[asn] === 'object' && asn !== 'unknown';
   }).forEach(function (asn) {
+    asns[domain][asn] = 0;
     Object.keys(asn_ip[asn]).filter(function (ip) {
       return ip !== 'empty' && ip !== 'undefined';
     }).filter(function (ip) {
@@ -43,6 +45,7 @@ function doDomain(into, line) {
     }).forEach(function (ip) {
       var classC = getClassC(ip);
 
+      asns[domain][asn] += asn_ip[asn][ip];
       into[classC] = into[classC] || {};
       into[classC][domain] = into[classC][domain] || 0;
       into[classC][domain] += asn_ip[asn][ip];
@@ -52,16 +55,18 @@ function doDomain(into, line) {
 
 function doAll() {
   var total = fs.statSync(inFile).size || 0,
-    into = {};
+    into = {},
+    asns = {};
 
   console.log(chalk.blue('Starting'));
   return Q.Promise(function (resolve, reject) {
     fs.createReadStream(inFile)
       .pipe(progress({total: total}))
       .pipe(es.split())
-      .pipe(es.mapSync(doDomain.bind({}, into)))
+      .pipe(es.mapSync(doDomain.bind({}, into, asns)))
       .on('end', function () {
         console.log(chalk.green('Done.'));
+        fs.writeFileSync(outfile1 + '.asncnts.json', JSON.stringify(asns));
         resolve(into);
       })
       .on('error', reject);
